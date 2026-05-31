e6a16e3af836706201883fca744ebcf9e484d02a
Vaishnavi Pawale <pawalevaishnsvi1@gmail.com>
2026-05-31 19:51:12 +0530

    added render url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+// To your actual live backend service URL:
+const BASE_URL = "https://doc-appointment-schedular.onrender.com";
+
+//const API_BASE_URL = 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
